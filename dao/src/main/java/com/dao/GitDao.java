8cfc16eb6704efeba78c310442e215b61eabbcd8
naruto <naruto@DESKTOP-GRFNSBT>
2019-01-29 09:08:04 +0800

    制造冲突

--- dao/src/main/java/com/dao/GitDao.java
+++ dao/src/main/java/com/dao/GitDao.java
@@ -7,8 +7,6 @@ public class GitDao {
 	public void update() {
 		System.out.println("完成修改方法");
 		System.out.println("完善修改方法");
-		System.out.println("�����޸ķ���");
-		System.out.println("完成修改方法");
-		System.out.println("完善修改方法");
+		System.out.println("更新修改方法");
 	}
 }
